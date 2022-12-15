f106dbb64f888edad5eec4d6cf19bffd1d4fcd19
riheme <rihemem@gotmail.com>
2022-12-15 11:43:44 +0100

    test

--- api/getName/index.js
+++ api/getName/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
 
-    const responseMessage = "This is a message from the Azure Function API yoohoo !"
+    const responseMessage = "TESThis is a message from the Azure Function API yoohoo !"
 
     context.res = {
         // status: 200, /* Defaults to 200 */
