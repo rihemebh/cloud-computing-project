b2a7ff0531541285d08edb8e86d67a853752e30c
riheme <rihemem@gotmail.com>
2022-12-15 03:15:09 +0100

    modify api

--- api/getName/index.js
+++ api/getName/index.js
@@ -1,8 +1,6 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = "This text is from the Azure Function API yoohoo !"
+    const responseMessage = "This is a message from the Azure Function API yoohoo !"
 
     context.res = {
         // status: 200, /* Defaults to 200 */
